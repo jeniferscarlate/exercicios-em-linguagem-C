2a0f6715978710175173d354e0e2287e3559656a
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-04-06 10:03:13 -0300

    Update 020_impar_par.cpp

--- exercícios/lista_exe_c/020_impar_par.cpp
+++ exercícios/lista_exe_c/020_impar_par.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main (){
+	//Declarando as variaveis:
 	int num = 0, par = 0;
 	
+	//Recebendo a entrada:
 	printf("Digite um numero: ");
 	scanf("%d", &num);
-	
+
+	//Processamento:	
 	par = (num%2);
+	
+	//Saida:
 	if(par == 0){
 		printf("o numero %d e par", num);
 	}
